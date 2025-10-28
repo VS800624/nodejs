d9f2ce7125de7c6688de83af283512a7a03a6b2a
VS800624 <vishalsingh800624@gmail.com>
2025-10-28 17:05:45 +0530

    learning about error handling and wild-card error handling

--- nodejs_Middlewares_and_errors_handlers/src/app.js
+++ nodejs_Middlewares_and_errors_handlers/src/app.js
@@ -1,4 +1,9 @@
 const express = require("express");
+
+// const auth = require("./middlewares/auth");
+// const adminAuth = auth.adminAuth;
+// const userAuth = auth.userAuth;
+// destructuring
 const { adminAuth, userAuth } = require("./middlewares/auth");
 
 const app = express();
@@ -9,24 +14,48 @@ const app = express();
 
 
 // handle auth middleware  for all get, post , , ...  etc requests
-app.use("/admin",adminAuth )
+// app.use("/admin", authAdmin)
 
-app.post("/user", (req,res) => {
-  res.send("User logged in successfully")
+// app.get("/user/login", authUser, (req,res,next) => {
+//   res.send("User logged in successfully")
+// }) 
+
+// app.post("/user/login", (req,res) => {
+//   res.send("User logged in successfully")
+// })
+
+// app.get("/admin/getAllData", (req,res) => {
+//   // logic of fetching all data
+//   res.send("All Data sent ")
+// })
+
+// app.get("/admin/deleteUser", (req,res) => {
+//   // logic for deleting a user
+//   res.send("Deleted a user")
+// })
+
+app.use("/", (err,req,res,next) => {
+  if(err) {
+    // log your error 
+    res.status(500).send("something went wrong")
+  }
 })
 
-app.get("/user", userAuth , (req,res) => {
-  res.send("User Data Sent")
-})
+app.get("/user",  (req,res) => {
+  try {
+    // logic of DB call and get user data 
+    throw new error("some random error")
+    res.send("User logged in successfully")
+  } catch (err) {
+    res.status(500).send("some error occurred please contact support team")
+  }
+}) 
 
-app.get("/admin/getAllData", (req,res) => {
-  // logic of fetching all data
-  res.send("All data sent")
-})
-
-app.get("/admin/deleteUser", (req,res) => {
-  // login for deleting a user
-  res.send("Deleted a User")
+app.use("/", (err,req,res,next) => {
+  if(err) {
+    // log your error 
+    res.status(500).send("something went wrong")
+  }
 })
 
 app.listen(3000, () => {
