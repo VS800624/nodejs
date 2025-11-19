3e5fe0cedaf9de1d99c25b8b5f5c4dbbd8d16159
VS800624 <vishalsingh800624@gmail.com>
2025-11-19 16:28:58 +0530

    added pagination in /feed API using skip and limit

--- Building_feed_API&Pagination/src/routes/user.js
+++ Building_feed_API&Pagination/src/routes/user.js
@@ -68,7 +68,7 @@ userRouter.get("/feed", userAuth, async(req,res) => {
     // 4) Already sent the connection request (i.e. interested)
     const loggedInUser = req.user
 
-    const page = parseInt(req.query.page) || 1
+   const page = parseInt(req.query.page) || 1
     let limit = parseInt(req.query.limit) || 10
     limit = limit>50 ? 50 : limit
     let skip = (page-1)*limit
